e0112bb3fdc0fd1f34b36c6319c197cd27fd9865
Vertika Pandey <vepandey@microsoft.com>
2023-02-15 22:53:44 +0530

    deployment changes

--- server/app.js
+++ server/app.js
@@ -3,13 +3,20 @@ const cors = require("cors");
 const app = express().use(express.json());
 app.use(cors());
 let message = "";
+const port = process.env.PORT;
 
-app.listen(3000, () => console.log("Server is up"));
+app.listen(port, () => console.log("Server is up"));
 
 app.post("/", (req, res) => {
   res.status(200).send({ message: message });
   message = "";
 });
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "hello" });
+  
+});
+
 
 app.post("/api/events", (req, res) => {
   console.log(req.body[0]);
